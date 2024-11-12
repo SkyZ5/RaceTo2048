7e111ec078e2b3ec053f3ca9c46016da873957ee
BT_2S14_15 <kmei0013@yahoo.com>
2024-11-12 11:02:19 -0500

    Fixed the bug in the squareAppear() method Working on the move up method Made a combine and create row method but the create row method is currently case sensitive, as in it only works for moving up. Combine should work no matter what though.

--- src/Game.java
+++ src/Game.java
@@ -9,7 +9,6 @@ public class Game {
         for(int i = 0; i < 16; i ++) {
             squares[i] = new Square(0, true);
         }
-        System.out.println(Arrays.toString(squares));
     }
     public void squareAppear(){
         Boolean works = false;
@@ -22,23 +21,69 @@ public class Game {
             randomValue = 2;
         }
         while (!(works)){
-            int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-            // int randomIndex = (int) (Math.random() * numbers.length);
-            int randomIndex = 1;
-            if (squares[numbers[randomIndex]].isEmpty()) {
-                squares[numbers[randomIndex]].giveValue(randomValue);
+            int[] numbers = new int[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+            ArrayList<Integer> arr_numbers = new ArrayList<Integer>();
+            for (int i : numbers)
+            {
+                arr_numbers.add(i);
+            }
+            int randomIndex = (int) (Math.random() * numbers.length);
+            if (squares[(arr_numbers.get(randomIndex))].isEmpty()) {
+                squares[arr_numbers.get(randomIndex)].giveValue(randomValue);
                 works = true;
                 System.out.println(randomIndex + ", " + randomValue);
             }
             else {
-                ArrayList<Integer> arr_new = new ArrayList<Integer>();
-                for (int i : numbers)
-                {
-                    arr_new.add(i);
-                }
-                arr_new.remove(randomIndex);
+                arr_numbers.remove(randomIndex);
             }
         }
-
+    }
+    public void moveUp() {
+        System.out.println(Arrays.toString(createRow(0)));
+        System.out.println(Arrays.toString(createRow(1)));
+        System.out.println(Arrays.toString(createRow(2)));
+        System.out.println(Arrays.toString(createRow(3)));
+    }
+    public String[] createRow(int integer) {
+        String[] strings = new String[4];
+        for (int i = 0; i < 15; i += 4) {
+            if (squares[i].isEmpty()){
+                strings[i / 4] = "empty";
+            }
+            else {
+                strings[i / 4] = squares[i].getValue() + "";
+            }
+        }
+        ArrayList<String> arr_strings = new ArrayList<String>();
+        for (String i : strings)
+        {
+            arr_strings.add(i);
+        }
+        return combine(arr_strings);
+    }
+    public String[] combine(ArrayList<String> arrayList){
+        while(arrayList.contains("empty")) {
+            arrayList.remove("empty");
+        }
+        for(int i = 0; i < arrayList.toArray().length; i ++) {
+            try {
+                if (Integer.parseInt(arrayList.get(i)) == Integer.parseInt(arrayList.get(i + 1))){
+                    arrayList.set(i, (Integer.parseInt(arrayList.get(i)) * 2) + "");
+                    arrayList.remove(i + 1);
+                }
+            }
+            catch (Exception e) {
+            }
+        }
+        String[] finalArray = new String[4];
+        for (int i = 0; i < 4;i ++) {
+            try {
+                finalArray[i] = arrayList.get(i);
+            }
+            catch (Exception e) {
+                finalArray[i] = "empty";
+            }
+        }
+        return finalArray;
     }
 }
--- src/Square.java
+++ src/Square.java
@@ -17,4 +17,7 @@ public class Square {
     public boolean isEmpty() {
         return empty;
     }
+    public int getValue(){
+        return value;
+    }
 }
